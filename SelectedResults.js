749477af2e0d1c0d332e31d609f2162e24e41d1e
Shraddha Shaligram <shraddha@Shraddhas-MacBook-Air.local>
2020-05-11 22:15:27 -0700

    hi

--- SelectedResults.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SelectedResult = ({ result }) => (
-  <a style={{ color: "red" }} href={result && result.url}>
-    {result && result.title}
-  </a>
-);
-
-export default SelectedResult;
