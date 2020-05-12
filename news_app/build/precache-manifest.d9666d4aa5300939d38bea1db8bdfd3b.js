76261e1cd97b16eab12053ef5090b3f8569d1b54
Shraddha Shaligram <shraddha@Shraddhas-MacBook-Air.local>
2020-05-11 22:40:16 -0700

    j

---
+++ news_app/build/precache-manifest.d9666d4aa5300939d38bea1db8bdfd3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cacef204332301cb79456b0d3d666503",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4479f112d9aaf8dcda8",
+    "url": "/static/css/2.f42e59c9.chunk.css"
+  },
+  {
+    "revision": "3dc83e2f6067dc0fb8de",
+    "url": "/static/css/main.17abf941.chunk.css"
+  },
+  {
+    "revision": "d4479f112d9aaf8dcda8",
+    "url": "/static/js/2.189ce0bf.chunk.js"
+  },
+  {
+    "revision": "30fff27b41afc8ef50fa01961033fec3",
+    "url": "/static/js/2.189ce0bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dc83e2f6067dc0fb8de",
+    "url": "/static/js/main.ca3354f2.chunk.js"
+  },
+  {
+    "revision": "988ddb1fd7dd84462017",
+    "url": "/static/js/runtime-main.ee3574bf.js"
+  }
+]);
